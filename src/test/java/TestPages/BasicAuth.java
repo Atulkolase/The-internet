962ee57f0cea1e46eeaa91011079f8e59dedc872
SupriyaBY <supriyabyadav.june22@gmail.com>
2023-12-14 17:28:42 +0530

    Authentication code added

---
+++ src/test/java/TestPages/BasicAuth.java
@@ -0,0 +1,20 @@
+package TestPages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicAuth {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://admin:admin@the-internet.herokuapp.com/");
+		
+		WebElement basicAuthEle = driver.findElement(By.xpath("//a[text()=\"Basic Auth\"]"));
+		basicAuthEle.click();
+		
+		}
+
+}
