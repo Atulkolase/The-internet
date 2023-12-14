c5f427fbd2816c3c407cf81ff3a84c20aedb2d04
Sagargb <sagargb9555@gmail.com>
2023-12-14 16:45:01 +0530

    DragAndDrop

---
+++ src/test/java/TestPages/Drag_Drop.java
@@ -0,0 +1,29 @@
+package TestPages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Drag_Drop {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://the-internet.herokuapp.com/drag_and_drop");
+		
+		
+		WebElement First = driver.findElement(By.xpath("//div[@id=\"column-a\"]"));
+		WebElement Second = driver.findElement(By.xpath("//div[@id=\"column-b\"]"));
+		
+		Actions act = new Actions(driver);
+		
+		act.dragAndDrop(Second, First).build().perform();
+		
+		
+		
+		
+	}
+
+}
