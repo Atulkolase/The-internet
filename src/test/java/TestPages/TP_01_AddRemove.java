0c168c5dc5213385476662e78a35bc0a1e4318ba
Atulkolase <atulkolase1009@gmail.com>
2023-12-14 16:06:22 +0530

    add remove element

---
+++ src/test/java/TestPages/TP_01_AddRemove.java
@@ -0,0 +1,32 @@
+package TestPages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TP_01_AddRemove {
+	
+	public static void main (String[]args) {
+		
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://the-internet.herokuapp.com/");
+		
+		WebElement addremoveEle = driver.findElement(By.xpath("//a[text()='Add/Remove Elements']"));
+		
+		addremoveEle.click();
+		
+		WebElement addbutton = driver.findElement(By.xpath("//button[text()='Add Element']"));
+		addbutton.click();
+		
+		WebElement deletbutton = driver.findElement(By.xpath("//button[text()='Delete']"));
+		deletbutton.click();
+		
+		addbutton.click();
+		
+//		driver.navigate().back();
+		
+	}
+}
+	
